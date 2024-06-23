768b5f0e49d62aa8981a186b7c4e32e7c5d96925
Antek <zapedowa@tcd.ie>
2024-06-23 16:56:16 +0200

    slow but working version

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import pandas
+import heapq
 
 def radius(population):
     METRO_CITY_POPULATION_CONSTANT = -1 / 1443000
@@ -12,12 +13,6 @@ def calcualate_metrocity_impact(max_radius, distance_to_metro_city):
     METRO_CITY_POWER_CONSTANT = -1.4
     impact = np.exp(METRO_CITY_POWER_CONSTANT * distance_to_metro_city / max_radius)
     return impact
-
-def load_data(filename):
-    data = pandas.read_csv(filename, delimiter='\t', engine='python')
-    settlements = data[data.iloc[:, 7] == 'ADM3']
-    metro = settlements[settlements.iloc[:, 14] >= 200000]
-    settlements = settlements.drop(metro.index)
 
 def calculate_distance(point_a_lat, point_a_lon, point_b_lat, point_b_lon):
     lat1 = math.radians(point_a_lat)
@@ -33,3 +28,9 @@ def calculate_distance(point_a_lat, point_a_lon, point_b_lat, point_b_lon):
 
     distance = 6371 * c
     return distance
+
+def find_n_smallest_indexes_2d(matrix, n):
+    flattened = [(matrix[i][j], i, j) for i in range(len(matrix)) for j in range(len(matrix[0]))]
+    smallest_with_indexes = heapq.nlargest(n, flattened)
+    smallest_indexes = [(row, col) for value, row, col in smallest_with_indexes]
+    return smallest_indexes
--- main.py
+++ main.py
@@ -5,45 +5,65 @@ def load_data(filename):
     global settlements
     data = pandas.read_csv(filename, delimiter='\t', engine='python')
     settlements = data[data.iloc[:, 7] == 'ADM3']
-    metro = settlements[settlements.iloc[:, 14] >= 200000]
+    metro = settlements[settlements.iloc[:, 14] >= 350000]
     settlements = settlements.drop(metro.index)
-
 
 def main():
     global metro
     global settlements
     load_data("PL.txt")
 
-    radius_metro = {}
 
-    for index, city in metro.iterrows():
-        city_name = city.iloc[2]
-        population = city.iloc[14]
-        radius_metro[city_name] = radius(population)
-    cities_to_metros = {}
+    max_value = 100
+    settlements_to_metros = {}
 
-    for index, settlement in settlements.iterrows():
-        for indext, city in metro.iterrows():
+    while max_value >= 0.2465969639416065:
 
-            settlement_lat = settlement.iloc[4]
-            settlement_lon = settlement.iloc[5]
-            metro_name = city.iloc[2]
-            metro_lat = city.iloc[4]
-            metro_lon = city.iloc[5]
-            distance = calculate_distance(settlement_lat, settlement_lon,metro_lat, metro_lon)
+        radius_metro = {}
+        for index, city in metro.iterrows():
+            city_name = city.iloc[2]
+            population = city.iloc[14]
+            radius_metro[city_name] = radius(population)
 
-            if distance < radius_metro[metro_name]:
+        arr = []
+        for index, settlement in settlements.iterrows():
+            subarr = []
+            for indext, city in metro.iterrows():
+                settlement_lat = settlement.iloc[4]
+                settlement_lon = settlement.iloc[5]
+                metro_lat = city.iloc[4]
+                metro_lon = city.iloc[5]
+                distance = calculate_distance(settlement_lat, settlement_lon,metro_lat, metro_lon)
+                curr_metro_radius = radius_metro[city.iloc[2]]
+                impact = calcualate_metrocity_impact(curr_metro_radius, distance)
+                subarr.append(impact)
+            arr.append(subarr)
 
-                settlement_name = settlement.iloc[2]
-                cities_to_metros[settlement_name] = metro_name
 
+        max_value = 0
+        row_index = 0
+        col_index = 0
+        for i, row in enumerate(arr):
+            for j, element in enumerate(row):
+                if element > max_value:
+                    row_index = i
+                    col_index = j
+                    max_value = element
 
-    print(cities_to_metros)
-    print(len(cities_to_metros))
+        #indexes = find_n_smallest_indexes_2d(arr, 20)
+        #print(f"The most impact this iteration is {max_value}")
+        #print(settlements.iloc[row_index, 2])
+        #print("is merged to")
+        #print(metro.iloc[col_index, 2])
 
-    print(calcualate_metrocity_impact(100, 1))
-    print(calcualate_metrocity_impact(100, 20))
+        settlements_to_metros[settlements.iloc[row_index, 2]] = metro.iloc[col_index, 2]
+        #print(metro.iloc[col_index, 14])
+        metro.iloc[col_index, 14] += settlements.iloc[row_index, 14]
+        #print(metro.iloc[col_index, 14])
+        settlements.drop(index= settlements.index[row_index], inplace=True)
+        print(max_value)
 
+    print(settlements_to_metros)
 
 if __name__ == "__main__":
     main()
