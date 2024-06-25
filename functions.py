1cc1e1d76a3cae5c9a429b514d2719137f853b25
Antek <zapedowa@tcd.ie>
2024-06-25 13:56:35 +0200

    bug fix, code comments

--- functions.py
+++ functions.py
@@ -3,16 +3,19 @@ import numpy as np
 import pandas
 import heapq
 
+
 def radius(population):
     METRO_CITY_POPULATION_CONSTANT = -1 / 1443000
     MIN_METRO_CITY_RADIUS = 10
     MAX_METRO_CITY_RADIUS = 100 - MIN_METRO_CITY_RADIUS
     return MIN_METRO_CITY_RADIUS + MAX_METRO_CITY_RADIUS * (1 - np.exp(METRO_CITY_POPULATION_CONSTANT * population))
 
+
 def calcualate_metrocity_impact(max_radius, distance_to_metro_city):
     METRO_CITY_POWER_CONSTANT = -1.4
     impact = np.exp(METRO_CITY_POWER_CONSTANT * distance_to_metro_city / max_radius)
     return impact
+
 
 def calculate_distance(point_a_lat, point_a_lon, point_b_lat, point_b_lon):
     lat1 = math.radians(point_a_lat)
@@ -29,11 +32,13 @@ def calculate_distance(point_a_lat, point_a_lon, point_b_lat, point_b_lon):
     distance = 6371 * c
     return distance
 
+
 def find_n_smallest_indexes_2d(matrix, n):
     flattened = [(matrix[i][j], i, j) for i in range(len(matrix)) for j in range(len(matrix[0]))]
     smallest_with_indexes = heapq.nlargest(n, flattened)
     smallest_indexes = [(row, col) for value, row, col in smallest_with_indexes]
     return smallest_indexes
+
 
 def first_digit_of_tuple(t):
     return int(str(t[0])[0])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from functions import *
 import time
 
+
+# retrieve all settlements and metrocities
 def load_data(filename):
     global metro
     global settlements
@@ -9,28 +11,31 @@ def load_data(filename):
     metro = settlements[settlements.iloc[:, 14] >= 300000]
     settlements = settlements.drop(metro.index)
 
+
 def main():
     start_time = time.time()
     global metro
     global settlements
     load_data("PL.txt")
 
-    #max_value = 100
+    # max_value = 100
     settlements_to_metros = {}
     interation_number = 1
-    #max_value >= 0.2465969639416065 and
+    # max_value >= 0.2465969639416065 and
 
     while len(settlements) != 0:
 
         before_settlements = len(settlements)
         before_settlements_to_metros = len(settlements_to_metros)
 
+        # calculate all radius of all metrocities
         radius_metro = {}
         for index, city in metro.iterrows():
             city_name = city.iloc[2]
             population = city.iloc[14]
             radius_metro[city_name] = radius(population)
 
+        # calculate impact of each settlement for each metrocity
         arr = []
         for index, settlement in settlements.iterrows():
             subarr = []
@@ -39,58 +44,53 @@ def main():
                 settlement_lon = settlement.iloc[5]
                 metro_lat = city.iloc[4]
                 metro_lon = city.iloc[5]
-                distance = calculate_distance(settlement_lat, settlement_lon,metro_lat, metro_lon)
+                distance = calculate_distance(settlement_lat, settlement_lon, metro_lat, metro_lon)
                 curr_metro_radius = radius_metro[city.iloc[2]]
                 impact = calcualate_metrocity_impact(curr_metro_radius, distance)
                 subarr.append(impact)
             arr.append(subarr)
 
+        # find n most impacted settlements
         number_of_indexes = 200
         indexes = find_n_smallest_indexes_2d(arr, number_of_indexes)
         indexes = sorted(indexes)
 
+        # try to merge n most impacted settlements with metrocities
         for i in indexes[:]:
             row_index = i[0]
-            if (str(settlements.iloc[row_index, 2])+ " " +str(settlements.iloc[row_index, 4]) + " " + str(settlements.iloc[row_index, 5]) ) in settlements_to_metros:
+            # ensure that the settlement wasn't already added this iteration
+            if (str(settlements.iloc[row_index, 2]) + " " + str(settlements.iloc[row_index, 4]) + " " + str(
+                    settlements.iloc[row_index, 5])) in settlements_to_metros:
                 indexes.remove(i)
             else:
                 col_index = i[1]
-                settlements_to_metros[(str(settlements.iloc[row_index, 2])+ " " +str(settlements.iloc[row_index, 4]) + " " + str(settlements.iloc[row_index, 5]))] = metro.iloc[col_index, 2]
+                settlements_to_metros[(
+                            str(settlements.iloc[row_index, 2]) + " " + str(settlements.iloc[row_index, 4]) + " " + str(
+                        settlements.iloc[row_index, 5]))] = metro.iloc[col_index, 2]
                 metro.iloc[col_index, 14] += settlements.iloc[row_index, 14]
 
-        if(len(settlements) > number_of_indexes):
-            for i in reversed(indexes):
-                row_index = i[0]
-                settlements.drop(index=settlements.index[row_index], inplace=True)
-        else:
-            settlements = {}
+        # remove n most impacted settlements already merged
+        #if (len(settlements) > number_of_indexes):
+        for i in reversed(indexes):
+            row_index = i[0]
+            settlements.drop(index=settlements.index[row_index], inplace=True)
+        #else:
+        #    settlements = {}
 
-        #print("Iteration nr. " + str(interation_number))
-        #print("Current hashmap len: " + str(len(settlements_to_metros)) + " added: " + str(len(settlements_to_metros)-before_settlements_to_metros))
-        #print("Current settlements len: " + str(len(settlements)) + " removed: " + str(before_settlements-len(settlements)))
-        #interation_number += 1
+        print("Iteration nr. " + str(interation_number))
+        print("Current hashmap len: " + str(len(settlements_to_metros)) + " added: " + str(len(settlements_to_metros)-before_settlements_to_metros))
+        print("Current settlements len: " + str(len(settlements)) + " removed: " + str(before_settlements-len(settlements)))
+        interation_number += 1
 
+    # print results
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Finish")
     print(f"Elapsed time: {elapsed_time} seconds")
-    print("Current hashmap len: " + str(len(settlements_to_metros)))
+    print("Current hashmap len: " + str(len(settlements_to_metros)) + " Amount of metrocities: " + str(len(metro)))
     print(settlements_to_metros)
-
-    sum = 0
-    for index, city in metro.iterrows():
-        sum += city.iloc[14]
-    print(sum)
+    #print(settlements)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
